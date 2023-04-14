a1ee0c2074deb68b57406c09269a2ab76c527983
Priyesh Rai <priyeshrai.delhi@gmail.com>
2023-04-15 03:16:26 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum{
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        System.out.println("------- Summation of two numbers -------")
+        System.out.println("Value of a = "+a+" || b = "+b);
+        int s=a+b;
+        System.out.println("Sum of these numbers is : "+s);
+    }
+}
